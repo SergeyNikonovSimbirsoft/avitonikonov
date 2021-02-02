b8e4e43b4a45e9554d2e61f97b9a478d103891b2
Sergey Nikonov <sergey.nikonov@simbirsoft.com>
2021-02-02 23:33:59 +0300

    убрано лого из personal

--- resources/views/layouts/profile.blade.php
+++ resources/views/layouts/profile.blade.php
@@ -64,19 +64,6 @@
                     @endauth
                 </div>
             </div>
-            <div class="container">
-                <div class="inner-header">
-                    <div class="row">
-                        <div class="col-lg-2 col-md-2">
-                            <div class="logo">
-                                <a href="{{ route('main') }}">
-                                    <img src="/img/logo.png" alt="">
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
             <div class="nav-item">
                 <div class="container">
                     <nav class="nav-menu mobile-menu">
